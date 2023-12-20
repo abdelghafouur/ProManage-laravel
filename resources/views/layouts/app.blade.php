3a43429ce86e4be627350c9706379dafce6fc092
abdelghafouur <LAHNIDA.ABDELGHAFOUR@ofppt-edu.ma>
2023-12-20 11:20:17 +0100

    add title&icone

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,12 +10,14 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>IMAYWEB - Agence Web Digitale à Marrakech Maroc</title>
 
     <meta name="description" content="" />
 
     <!-- Favicon -->
-    <link rel="icon" type="image/x-icon" />
+    <link rel="icon" href="{{ asset('assets/img/image/icone_logo.png') }}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ asset('assets/img/image/icone_logo.png') }}" type="image/x-icon">
+
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com" />
